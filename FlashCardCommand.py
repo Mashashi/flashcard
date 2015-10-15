454ff907fa27f629fcbfd300a4ca87f1fb5caef1
Mashashi Nakamura <mashashi.nakamura@gmail.com>
2015-10-16 00:39:55 +0200

    Stabilization and usability improvements
    + Change the shortcut keys
    + Grooming on the code
    + Change the separator to tab instead of space
    + Add a new line after the question is printed
    + Added further information on the README.md file
    + Validated input
    + Stress tests & corrections detected

--- FlashCardCommand.py
+++ FlashCardCommand.py
@@ -1,68 +1,85 @@
-import sublime, sublime_plugin, random, math
+import sublime, sublime_plugin, random, math, re
+
+FLASH_CARD_SEP = "\n"
+LIST_FLASH_CARDS_SEP = "\n"
+LIST_FLASH_CARD_SEP = "\t"
 
 class FlashCardCommand(sublime_plugin.TextCommand):
 
 	sels = []
 	line_count = 0
-	new_view = None
 
 	def run(self, edit, get_selection=False):
-		if get_selection:
-			
-			body = self.view.substr(sublime.Region(0, self.view.size()))
-			body = body.split('\n')
-			question = body[0]
-			reply = body[1]
-
-			result = FlashCardCommand.checkAnswer(question, reply)
-
-			if result:
-				FlashCardCommand.initQuestion(FlashCardCommand.new_view, edit)
-
-		else:
-			FlashCardCommand.new_view = FlashCardCommand.setup(self.view, edit)
-			FlashCardCommand.initQuestion(FlashCardCommand.new_view, edit)
-
-
-
-
-
-	def initQuestion(view, edit):
-		view.erase(edit, sublime.Region(0, view.size()))
-		line = FlashCardCommand.getLine()
-		question = line.split(' ')[0]
-		view.insert(edit, 0, question)
-
-	def checkAnswer(question, reply):
-		result = False
-		for sel in FlashCardCommand.sels:
-			
-			sel = sel.split(' ')
-			key = sel[0]
-			value = sel[1]
-			
-			if question == key:
-				if reply == value:
-					sublime.status_message('correct')
-					result = True
-				else:
-					sublime.status_message('wrong')
-					result = False
-
-		return result
-
-	def setup(view, edit):
-		window = sublime.active_window()
-		window.new_file()
-		new_view = window.active_view()
-		sels = view.sel()
-		sels = view.substr(sels[0])
-		FlashCardCommand.sels = sels.split("\n")
-		FlashCardCommand.line_count = len(FlashCardCommand.sels)
-		return new_view
 		
-	def getLine():
-		random_number = random.random()
-		pick_idx = math.floor(random_number*FlashCardCommand.line_count)
-		pick_line = FlashCardCommand.sels[pick_idx]
-		return pick_line
+		if get_selection:
+			body = getBodyFlashCard(self.view)
+			line = Line(body, FLASH_CARD_SEP)
+			result = newFlashCard(line.question, line.reply)
+			if result: initQuestion(edit)
+		else:
+			if validSelection(self.view):
+				new_view = setup(self.view, edit)
+				initQuestion(edit, new_view)
+			else:
+				sublime.status_message("invalid selection")
+
+def getBody(view):
+	body = view.substr(sublime.Region(0, view.size()))
+	return body
+
+def getBodyFlashCard(view):
+	body = view.substr(sublime.Region(0, view.size()))
+	if body.count(FLASH_CARD_SEP) != 1: body = FLASH_CARD_SEP
+	return body
+
+def getSelection(view):
+	return view.substr(view.sel()[0])
+
+def initQuestion(edit, view = None):
+	if view == None: view = sublime.active_window().active_view()
+	view.erase(edit, sublime.Region(0, view.size()))
+	view.insert(edit, 0, getLine().flash_card)
+
+def newFlashCard(question, reply):
+	response = None
+
+	for sel in FlashCardCommand.sels:
+		if question == sel.question:
+			response = False
+			if reply == sel.reply:
+				response = True
+				break
+	
+	if response != None: sublime.status_message( 'correct' if response else 'wrong' )
+	if response == None: response = True
+
+	return response
+
+def setup(view, edit):
+	window = sublime.active_window()
+	window.new_file()
+	new_view = window.active_view()
+	sels = getSelection(view)
+	FlashCardCommand.sels = list(map(Line, sels.split(LIST_FLASH_CARDS_SEP)))
+	FlashCardCommand.line_count = len(FlashCardCommand.sels)
+	return new_view
+
+def getLine():
+	pick_idx = math.floor(random.random()*FlashCardCommand.line_count)
+	picked_line = FlashCardCommand.sels[pick_idx]
+	return picked_line
+
+def validSelection(view):
+	body = getSelection(view)
+	valid = False
+	parts = body.split(LIST_FLASH_CARDS_SEP)
+	for part in parts:
+		valid = part.count(LIST_FLASH_CARD_SEP) == 1
+		if not(valid): break
+	return valid
+
+class Line:
+	def __init__(self, line, separator = LIST_FLASH_CARD_SEP):
+		if not line: line = separator
+		[self.question, self.reply] = line.split(separator)
+		self.flash_card = self.question + FLASH_CARD_SEP
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # flashcard
-A plugin for sublime that helps to memoraize like you would with flash cards
+
+A plugin for sublime that helps to memoraize like you would with flash cards.
+
+## Usage
+
+1. Type a pair of words separated by a tab in each line of a file.
+	
+	The 1st sequence of the characters is the question.
+	The 2nd is the expected reply.
+
+2. Select the lines that you would like to see as question on the flash card
+
+3. Press ctrl+alt+s
+
+4. A new tab appears with a random selected question.
+
+5. Type the reply you think is correct on the second line.
+
+6. Press ctrl+alt+space to check your answer.
+
+	On the status bar at the end you will see one of two things:
+	+ "wrong" and the question doesn't change
+	+ "correct" and a new question appears
+
+##Roadmap
+
+* Limitation of time per flashcard
+
+* Get the minimum distance edit ocorrence after 5 wrongs
+
+* Show wrongs more often
+
+##Observations
+
+* You don't have several instances of the flash cards you get only one. 
+
+* You are not forced to run the flash cards in the file that opens. You can hit ctrl+alt+space and all text in that document will be deleted and you will be presented with a flash card.
+
+* You may get the response correct and the second flash card be the same.
+
+* If you don't make a valid selection you will see the message invalid selection on the status bar and the flashcards plugin will not load the data.
